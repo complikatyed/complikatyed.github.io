0eb0f2657326835258178ec2cd5a50e2ba0030fe
complikatyed <katye.russell@gmail.com>
2016-09-05 23:13:18 -0500

    Add projects controller and ng-repeat to display projects with search option

--- app/controllers/ProjectsCtrl.js
+++ app/controllers/ProjectsCtrl.js
@@ -1,7 +1,63 @@
 "use strict";
-app.controller("ProjectCtrl", function($scope, $location){
+app.controller("ProjectsCtrl", function($scope, $location){
 
+  $scope.projects = [
 
-
+    {
+      "projectName": "Tic Taco",
+      "repoLink": "https://github.com/complikatyed/complikatyed.github.io",
+      "describe": "Group project for NSS C8 - two player Tic-Tac-Toe game",
+      "techs": [ "HTML", "CSS", "Sass", "Gulp", "Angular", "JSON", "Firebase", "Materialize" ],
+      "solo": false,
+      "team": [ "Andy Meagher", "Margaret Clinard"],
+      "img1": "https://raw.githubusercontent.com/complikatyed/TicTacToe/master/app/img/TicTaco1.png",
+      "img2": "https://github.com/complikatyed/TicTacToe/blob/master/app/img/TicTaco2.png",
+      "img3": "https://github.com/complikatyed/TicTacToe/blob/master/app/img/TicTaco3.png"
+    },
+    {
+      "projectName": "ng-Address",
+      "repoLink": "https://github.com/complikatyed/ng-address-app",
+      "describe": "Angular-based address book using Materialize for styling",
+      "techs": [ "HTML", "CSS", "Sass", "Angular", "JSON", "Firebase", "Materialize" ],
+      "solo": true,
+      "team": "",
+      "img1": "https://raw.githubusercontent.com/complikatyed/TicTacToe/master/app/img/TicTaco2.png",
+      "img2": "",
+      "img3": ""
+    },
+    {
+      "projectName": "Movie History",
+      "repoLink": "https://github.com/complikatyed/Movie-History",
+      "describe": "Group project for NSS E3 class using OMDB API and Firebase",
+      "techs": [ "HTML", "CSS", "Sass", "Angular", "JSON", "Firebase", "Materialize" ],
+      "solo": false,
+      "team": [ "Tim Maddux", "Chris Barrett" ],
+      "img1": "https://raw.githubusercontent.com/complikatyed/Movie-History/master/img/AlreadyWatchedMovies.png",
+      "img2": "https://github.com/complikatyed/Movie-History/blob/master/img/SearchResults.png",
+      "img3": "https://github.com/complikatyed/Movie-History/blob/master/img/UnwatchedMovies.png"
+    },
+        {
+      "projectName": "Movie History",
+      "repoLink": "https://github.com/complikatyed/Movie-History",
+      "describe": "Group project for NSS E3 class using OMDB API and Firebase",
+      "techs": [ "HTML", "CSS", "Sass", "Angular", "JSON", "Firebase", "Materialize" ],
+      "solo": false,
+      "team": [ "Tim Maddux", "Chris Barrett" ],
+      "img1": "https://raw.githubusercontent.com/complikatyed/Movie-History/master/img/AlreadyWatchedMovies.png",
+      "img2": "https://github.com/complikatyed/Movie-History/blob/master/img/SearchResults.png",
+      "img3": "https://github.com/complikatyed/Movie-History/blob/master/img/UnwatchedMovies.png"
+    },
+        {
+      "projectName": "Movie History",
+      "repoLink": "https://github.com/complikatyed/Movie-History",
+      "describe": "Group project for NSS E3 class using OMDB API and Firebase",
+      "techs": [ "HTML", "CSS", "Sass", "Angular", "JSON", "Firebase", "Materialize" ],
+      "solo": false,
+      "team": [ "Tim Maddux", "Chris Barrett" ],
+      "img1": "https://raw.githubusercontent.com/complikatyed/Movie-History/master/img/AlreadyWatchedMovies.png",
+      "img2": "https://github.com/complikatyed/Movie-History/blob/master/img/SearchResults.png",
+      "img3": "https://github.com/complikatyed/Movie-History/blob/master/img/UnwatchedMovies.png"
+    }
+  ];
 
 });
